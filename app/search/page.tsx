d6401d18b64d11b6fcb8900b19b93b0f6e6aa621
Anjiwoong <rlqmatk@naver.com>
2023-08-03 21:28:42 +0900

    Feat: Section6 / 53

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,15 +1,96 @@
+import { PRICE, PrismaClient } from '@prisma/client';
+
 import Header from './components/Header';
 import RestaurantCard from './components/RestaurantCard';
 import SearchSideBar from './components/SearchSideBar';
 
-const Search = () => {
+const prisma = new PrismaClient();
+
+interface SearchParams {
+  city?: string;
+  cuisine?: string;
+  price?: PRICE;
+}
+
+const fetchRestaurantByCity = (searchParams: SearchParams) => {
+  const where: any = {};
+
+  if (searchParams.city) {
+    const location = {
+      name: {
+        equals: searchParams.city.toLowerCase(),
+      },
+    };
+    where.location = location;
+  }
+
+  if (searchParams.cuisine) {
+    const cuisine = {
+      name: {
+        equals: searchParams.cuisine.toLowerCase(),
+      },
+    };
+    where.cuisine = cuisine;
+  }
+
+  if (searchParams.price) {
+    const price = {
+      name: {
+        equals: searchParams.price,
+      },
+    };
+    where.price = price;
+  }
+
+  const select = {
+    id: true,
+    name: true,
+    main_image: true,
+    price: true,
+    cuisine: true,
+    location: true,
+    slug: true,
+    reviews: true,
+  };
+
+  return prisma.restaurant.findMany({
+    where,
+    select,
+  });
+};
+
+const fetchLocations = async () => {
+  return prisma.location.findMany();
+};
+
+const fetchCuisine = async () => {
+  return prisma.cuisine.findMany();
+};
+
+const Search = async ({ searchParams }: { searchParams: SearchParams }) => {
+  const restaurants = await fetchRestaurantByCity(searchParams);
+  const location = await fetchLocations();
+  const cuisine = await fetchCuisine();
+
   return (
     <>
       <Header />
       <div className="flex py-4 m-auto w-2/3 justify-between items-start">
-        <SearchSideBar />
+        <SearchSideBar
+          locations={location}
+          cuisines={cuisine}
+          searchParams={searchParams}
+        />
         <div className="w-5/6">
-          <RestaurantCard />
+          {restaurants.length ? (
+            <>
+              {restaurants.map((restaurant) => (
+                <RestaurantCard restaurant={restaurant} key={restaurant.id} />
+              ))}
+            </>
+          ) : (
+            <p>Sorry, we found no restaurants in this area</p>
+          )}
         </div>
       </div>
     </>
